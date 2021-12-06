929fda43c10c7300e8148e9d4b7ab08084be2393
Benson Ngoy <bngoy@redhat.com>
2021-12-06 17:44:13 +0100

    Simplifying init for cloudclient

--- cmd/byovpc/cmd.go
+++ cmd/byovpc/cmd.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"os"
 
-	configv1 "github.com/openshift/api/config/v1"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/openshift/osd-network-verifier/pkg/cloudclient"
 	"github.com/spf13/cobra"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
@@ -14,20 +14,11 @@ func NewCmdByovpc(streams genericclioptions.IOStreams) *cobra.Command {
 	byovpcCmd := &cobra.Command{
 		Use: "byovpc",
 		Run: func(cmd *cobra.Command, args []string) {
-			// Do Stuff Here
-			// AWS or GCP
+			creds := credentials.NewStaticCredentialsProvider(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), os.Getenv("AWS_SESSION_TOKEN"))
+			region := os.Getenv("AWS_DEFAULT_REGION")
+			cli, err := cloudclient.NewClient(creds, region)
 
-			caller := configv1.AWSPlatformType // testing only, assuming it's aws, check what was provided actually.
-			var cli cloudclient.CloudClient
-
-			switch {
-			case caller == configv1.AWSPlatformType:
-				cli = cloudclient.GetClientFor(configv1.AWSPlatformType)
-			case caller == configv1.GCPPlatformType:
-				cli = cloudclient.GetClientFor(configv1.GCPPlatformType)
-			}
-
-			err := cli.ByoVPCValidator(context.TODO())
+			err = cli.ByoVPCValidator(context.TODO())
 			if err != nil {
 				streams.ErrOut.Write([]byte(err.Error()))
 				os.Exit(1)
