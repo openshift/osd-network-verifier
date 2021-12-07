08dfb0e503d71673b482a3a211b32d4e6c5e2dba
lnguyen1401 <linnguye@redhat.com>
2021-12-07 15:08:32 -0500

    added aws_test.go

---
+++ pkg/cloudclient/aws/aws_test.go
@@ -0,0 +1,22 @@
+package aws
+
+import (
+	"testing"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+)
+
+func TestNewClient(t *testing.T) {
+
+	creds := credentials.NewStaticCredentialsProvider("dummyID", "dummyPassKey", "dummyToken")
+	region := "us-east-1"
+	cli, err := NewClient(creds, region)
+
+	if err != nil {
+		t.Error("err occured while creating cli:", err)
+	}
+
+	if cli == nil {
+		t.Errorf("cli should have been initialized")
+	}
+}
+
