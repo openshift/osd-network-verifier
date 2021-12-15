b1f70cd6c390551c3275b651b6b7fcfd4a812624
Max Whittingham <mwhittin@redhat.com>
2021-12-15 16:47:31 -0700

    Small Readme updates, initial pass at GCP unit tests

--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/credentials v1.6.4
 	github.com/aws/aws-sdk-go-v2/service/ec2 v1.24.0
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/mock v1.5.0
+	github.com/golang/mock v1.6.0
 	github.com/openshift-online/ocm-sdk-go v0.1.224
 	github.com/openshift/api v0.0.0-20211108165917-be1be0e89115
 	github.com/spf13/cobra v1.2.1
---
+++ pkg/cloudclient/gcp/gcp_test.go
@@ -0,0 +1,58 @@
+package gcp
+
+import (
+	"context"
+	"testing"
+
+	gomock "github.com/golang/mock/gomock"
+	mock_cloudclient "github.com/openshift/osd-network-verifier/pkg/cloudclient/mock_cloudclient"
+
+	ocmlog "github.com/openshift-online/ocm-sdk-go/logging"
+	"golang.org/x/oauth2/google"
+)
+
+func TestByoVPCValidator(t *testing.T) {
+	ctx := context.TODO()
+	mock := mock_cloudclient.NewMockCloudClient(gomock.NewController(t))
+	mock.EXPECT().ByoVPCValidator(ctx).Return(nil)
+	err := mock.ByoVPCValidator(ctx)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
+
+func TestValidateEgress(t *testing.T) {
+	ctx := context.TODO()
+	subnetID := "subnet-id"
+	cloudImageID := "image-id"
+	mock := mock_cloudclient.NewMockCloudClient(gomock.NewController(t))
+	mock.EXPECT().ValidateEgress(ctx, subnetID, cloudImageID).Return(nil)
+	err := mock.ValidateEgress(ctx, subnetID, cloudImageID)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
+
+func TestNewClient(t *testing.T) {
+	ctx := context.TODO()
+	logger, err := ocmlog.NewStdLoggerBuilder().Error(true).Debug(true).Build()
+	if err != nil {
+		println("unexpected error creating logger: %v", err)
+	}
+	credentials := &google.Credentials{ProjectID: "my-sample-project-191923"}
+	region := "superstable-region1-z"
+	tags := map[string]string{"osd-network-verifier": "owned"}
+	client, err := NewClient(ctx, logger, credentials, region, tags)
+	if err != nil {
+		t.Errorf("unexpected error creating client: %v", err)
+	}
+	if client.projectID != credentials.ProjectID {
+		t.Errorf("unexpected project ID: %v", client.projectID)
+	}
+	if client.region != region {
+		t.Errorf("unexpected region: %v", client.region)
+	}
+	if client.tags["osd-network-verifier"] != "owned" {
+		t.Errorf("unexpected tags: %v", client.tags)
+	}
+}
