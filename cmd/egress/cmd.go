a901b0302488a900b1d0114eb455cb771aa13c89
Benson Ngoy <bngoy@redhat.com>
2021-12-16 14:25:40 +0100

    cross-platform userdata

--- cmd/egress/cmd.go
+++ cmd/egress/cmd.go
@@ -11,8 +11,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var defaultTags = map[string]string{
-	"osd-network-verifier": "owned",
+var (
+	defaultTags            = map[string]string{"osd-network-verifier": "owned"}
+	regionEnvVarStr string = "AWS_DEFAULT_REGION"
+	regionDefault   string = "us-east-2"
+)
+
+func getDefaultRegion() string {
+	val, present := os.LookupEnv(regionEnvVarStr)
+	if present {
+		return val
+	} else {
+		return regionDefault
+	}
 }
 
 func NewCmdValidateEgress() *cobra.Command {
@@ -20,6 +31,7 @@ func NewCmdValidateEgress() *cobra.Command {
 	var cloudImageID string
 	var cloudTags map[string]string
 	var debug bool
+	var region string
 
 	validateEgressCmd := &cobra.Command{
 		Use: "egress",
@@ -36,15 +48,8 @@ func NewCmdValidateEgress() *cobra.Command {
 			ctx := context.TODO()
 
 			creds := credentials.NewStaticCredentialsProvider(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), os.Getenv("AWS_SESSION_TOKEN"))
-			// TODO this should probably be a command-line option that overwrites the env var
-			regionEnvVarStr := "AWS_DEFAULT_REGION"
-			regionDefault := "us-east-2"
-			region := os.Getenv(regionEnvVarStr)
-			if len(region) < 1 {
-				logger.Warn(ctx, "No region defined in %s env, defaulting to %s", regionEnvVarStr, regionDefault)
-				region = regionDefault
-			}
 
+			logger.Warn(ctx, "Using region: %s", region)
 			cli, err := cloudclient.NewClient(ctx, logger, creds, region, cloudTags)
 			if err != nil {
 				logger.Error(ctx, err.Error())
@@ -64,6 +69,7 @@ func NewCmdValidateEgress() *cobra.Command {
 
 	validateEgressCmd.Flags().StringVar(&vpcSubnetID, "subnet-id", "", "ID of the source subnet")
 	validateEgressCmd.Flags().StringVar(&cloudImageID, "image-id", "", "ID of cloud image")
+	validateEgressCmd.Flags().StringVar(&region, "region", getDefaultRegion(), fmt.Sprintf("Region to validate. Defaults to exported var %[1]v or '%[2]v' if not %[1]v set", regionEnvVarStr, regionDefault))
 	validateEgressCmd.Flags().StringToStringVar(&cloudTags, "cloud-tags", defaultTags, "Comma-seperated list of tags to assign to cloud resources")
 	validateEgressCmd.Flags().BoolVar(&debug, "debug", false, "If true, enable additional debug-level logging")
 	validateEgressCmd.MarkFlagRequired("subnet-id")
