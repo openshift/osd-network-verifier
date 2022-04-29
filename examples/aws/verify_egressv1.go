f7d6af21468fc084b0129f12b2e846b0ce00ea31
Ritu Mundhe <rmundhe@redhat.com>
2022-04-29 15:16:20 -0500

    updated examples with comments

--- examples/aws/verify_egressv1.go
+++ examples/aws/verify_egressv1.go
@@ -11,31 +11,31 @@ import (
 	"github.com/openshift/osd-network-verifier/pkg/cloudclient"
 )
 
+//Example config struct
 type egressConfigV1 struct {
 	debug bool
 }
 
 // Use egress validator
 func extendValidateEgressV1(ctx context.Context) error {
-	//initialize required args
-	//---------
+	//---------initialize required args---------
 	//read AWS creds from environment
 	key, _ := os.LookupEnv("AWS_ACCESS_KEY_ID")
 	secret, _ := os.LookupEnv("AWS_SECRET_ACCESS_KEY")
 	session, _ := os.LookupEnv("AWS_SESSION_TOKEN")
-	//build the v2 credentials
+	//build the v1 credentials
 	creds := credentials.NewStaticCredentials(key, secret, session)
-	region := "us-east-1"
-	instanceType := "m5.2xlarge"
-	tags := make(map[string]string)
-	tags["key1"] = "val1"
 	builder := ocmlog.NewStdLoggerBuilder()
 	config := egressConfigV1{}
 	builder.Debug(config.debug)
 	logger, _ := builder.Build()
-	//---------
+	//example required values
+	region := "us-east-1"
+	instanceType := "m5.2xlarge"
+	tags := make(map[string]string)
+	tags["key1"] = "val1"
 
-	// init cloudclient
+	//---------ONV egress verifier usage---------
 	cli, _ := cloudclient.NewClient(ctx, logger, *creds, region, instanceType, tags)
 	// Call egress validator
 	out := cli.ValidateEgress(context.TODO(), "vpcSubnetID", "cloudImageID", "kmsKeyID", 600)
