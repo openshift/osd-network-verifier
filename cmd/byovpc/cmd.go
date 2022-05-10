6c14cde6ac9fabbb34c6e377252d18aac932219a
Ritu Mundhe <rmundhe@redhat.com>
2022-05-10 13:16:04 -0500

    Osd 10346   PR for ONV doc enhancements   (#61)
    * OSD-10346 Enhancing cli help doc
    
    * OSD-10346 Expanded AWS egress verification doc
    
    * Fixed #3 in workflow
    Added common instructions to main README.md
    
    * Fixed toc link
    
    * instructions for getting AWS creds
    
    * SOP for getting AWS creds
    
    * SOP for getting AWS creds
    
    * fixed code block
    
    * added step
    
    * added headings
    
    * updated readme
    
    * updated readme
    
    * removed build cli from setup. put it under cli.
    
    * nitpicking
    
    * - removed unnecessary dependency, private SOP info
    - phrasing per review
    - separated implementation examples
    
    * updated toc to reflect headings
    
    * nitpick
    
    * added to main README
    
    * correction
    
    Co-authored-by: Taylor Fahlman <fahlmantaylor@gmail.com>
    
    * correction
    
    Co-authored-by: Taylor Fahlman <fahlmantaylor@gmail.com>
    
    * updated examples with comments
    
    * updated examples with comments
    
    * show minimum required flags in exec, with description of optional available flags
    
    * updated logger creation
    
    * removing unused argument
    
    * Removed unused structs
    
    * fixed error
    
    * Update README_AWS.md
    
    Co-authored-by: Taylor Fahlman <fahlmantaylor@gmail.com>
    
    * Update README.md
    
    Co-authored-by: Taylor Fahlman <fahlmantaylor@gmail.com>
    
    * Update cmd/byovpc/cmd.go
    
    Co-authored-by: Taylor Fahlman <fahlmantaylor@gmail.com>
    
    * fix "prior to". Can be used after as well.
    
    * formatting
    
    Co-authored-by: Taylor Fahlman <fahlmantaylor@gmail.com>

--- cmd/byovpc/cmd.go
+++ cmd/byovpc/cmd.go
@@ -16,6 +16,7 @@ var debug bool
 func NewCmdByovpc() *cobra.Command {
 	byovpcCmd := &cobra.Command{
 		Use: "byovpc",
+		Short: "Verify subnet configuration of a specific VPC",
 		Run: func(cmd *cobra.Command, args []string) {
 			// Create logger
 			builder := ocmlog.NewStdLoggerBuilder()
