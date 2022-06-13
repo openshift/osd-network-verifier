964a385b1f01f787a0838ad360ea0fcf06af0328
Ritu Mundhe <rmundhe@redhat.com>
2022-06-13 12:39:27 -0500

    fixed examples per new function signature

--- examples/aws/verify_egressv1.go
+++ examples/aws/verify_egressv1.go
@@ -3,32 +3,24 @@ package aws
 import (
 	"context"
 	"fmt"
-	"os"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	ocmlog "github.com/openshift-online/ocm-sdk-go/logging"
 	"github.com/openshift/osd-network-verifier/pkg/cloudclient"
 )
 
 func extendValidateEgressV1() {
 	//---------Initialize required args---------
-	// Read AWS creds from environment
-	key, _ := os.LookupEnv("AWS_ACCESS_KEY_ID")
-	secret, _ := os.LookupEnv("AWS_SECRET_ACCESS_KEY")
-	session, _ := os.LookupEnv("AWS_SESSION_TOKEN")
 
-	// Build the v1 credentials
-	creds := credentials.NewStaticCredentials(key, secret, session)
-
-	// Example required values
+	// Example values
 	logger, _ := ocmlog.NewStdLoggerBuilder().Debug(true).Build()
 	region := "us-east-1"
 	instanceType := "m5.2xlarge"
 	tags := map[string]string{"key1": "val1"}
 
 	//---------ONV egress verifier usage---------
-	cli, _ := cloudclient.NewClient(context.TODO(), logger, *creds, region, instanceType, tags)
+	cli, _ := cloudclient.NewClient(context.TODO(), logger, region, instanceType, tags, "aws",
+		"")
 	// Call egress validator
 	out := cli.ValidateEgress(context.TODO(), "vpcSubnetID", "cloudImageID", "kmsKeyID", 3*time.Second)
 	if !out.IsSuccessful() {
--- examples/aws/verify_egressv2.go
+++ examples/aws/verify_egressv2.go
@@ -3,33 +3,24 @@ package aws
 import (
 	"context"
 	"fmt"
-	"os"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/credentials"
 	ocmlog "github.com/openshift-online/ocm-sdk-go/logging"
 	"github.com/openshift/osd-network-verifier/pkg/cloudclient"
 )
 
 func extendValidateEgressV2() {
-	ctx := context.TODO()
 	//---------initialize required args---------
-	// Read AWS creds from environment
-	key, _ := os.LookupEnv("AWS_ACCESS_KEY_ID")
-	secret, _ := os.LookupEnv("AWS_SECRET_ACCESS_KEY")
-	session, _ := os.LookupEnv("AWS_SESSION_TOKEN")
 
-	// Build the v2 credentials provider
-	creds := credentials.NewStaticCredentialsProvider(key, secret, session)
-
-	// Example required values
+	// Example values
 	logger, _ := ocmlog.NewStdLoggerBuilder().Debug(true).Build()
 	region := "us-east-1"
 	instanceType := "m5.2xlarge"
 	tags := map[string]string{"key1": "val1"}
 
 	//---------ONV egress verifier usage---------
-	cli, _ := cloudclient.NewClient(ctx, logger, creds, region, instanceType, tags)
+	cli, _ := cloudclient.NewClient(context.TODO(), logger, region, instanceType, tags, "aws",
+		"")
 	// Call egress validator
 	out := cli.ValidateEgress(context.TODO(), "vpcSubnetID", "cloudImageID", "kmsKeyID", 3*time.Second)
 	if !out.IsSuccessful() {
