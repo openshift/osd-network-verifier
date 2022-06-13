290f6c44827ee878efde68f30fd121fc33d504bb
Ritu Mundhe <rmundhe@redhat.com>
2022-06-13 15:27:04 -0500

    revert error string regex change

--- pkg/cloudclient/aws/private.go
+++ pkg/cloudclient/aws/private.go
@@ -271,7 +271,7 @@ func generateUserData(variables map[string]string) (string, error) {
 func (c *Client) findUnreachableEndpoints(ctx context.Context, instanceID string) error {
 	// Compile the regular expressions once
 	reVerify := regexp.MustCompile(userdataEndVerifier)
-	reUnreachableErrors := regexp.MustCompile(`((\bUnable to reach\s)(\w+))`)
+	reUnreachableErrors := regexp.MustCompile(`Unable to reach (\S+)`)
 
 	latest := true
 	input := ec2.GetConsoleOutputInput{
