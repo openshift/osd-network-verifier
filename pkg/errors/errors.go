9426abb8d11e54edf12c771d95d8cb965df10946
Ritu Mundhe <rmundhe@redhat.com>
2022-06-13 16:14:51 -0500

    reverting trivial changes

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -5,16 +5,31 @@ import (
 	"fmt"
 )
 
-var ErrWaitTimeout = errors.New("timed out waiting for the condition")
-
 type EgressURLError struct {
 	e string
 }
 
+var ErrWaitTimeout = errors.New("timed out waiting for the condition")
+
+func (e *GenericError) ErrWaitTimeout() string { return e.message }
+
 func (e *EgressURLError) Error() string { return e.e }
+
 func NewEgressURLError(failure string) error {
 	return &EgressURLError{
-		e: fmt.Sprintf("egressURL error: Unable to reach %s", failure),
+		e: fmt.Sprintf("egressURL error: %s", failure),
+	}
+}
+
+type GenericError struct {
+	message string
+}
+
+func (e *GenericError) Error() string { return e.message }
+
+func NewGenericError(message string) error {
+	return &GenericError{
+		message: fmt.Sprintf("network verifier error: %s", message),
 	}
 }
 
@@ -27,16 +42,5 @@ func (e *UnhandledError) ErrWaitTimeout() string { return e.e }
 func NewGenericUnhandledError(err error) error {
 	return &UnhandledError{
 		e: fmt.Sprintf("generic unhandled error: %s ", err.Error()),
-	}
-}
-
-type GenericError struct {
-	message string
-}
-
-func (e *GenericError) Error() string { return e.message }
-func NewGenericError(message string) error {
-	return &GenericError{
-		message: fmt.Sprintf("network verifier error: %s", message),
 	}
 }
