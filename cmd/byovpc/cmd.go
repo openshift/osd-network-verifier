011839a7a63488179e197fc2b4888a42aa6add88
Ritu Mundhe <rmundhe@redhat.com>
2022-07-07 20:27:59 -0500

    updated DNS and BYOVPC to match Egress cli

--- cmd/byovpc/cmd.go
+++ cmd/byovpc/cmd.go
@@ -1,49 +1,17 @@
 package byovpc
 
 import (
-	"context"
-	"fmt"
-	"os"
-
-	ocmlog "github.com/openshift-online/ocm-sdk-go/logging"
-	"github.com/openshift/osd-network-verifier/pkg/cloudclient"
 	"github.com/spf13/cobra"
 )
 
 var debug bool
 
 func NewCmdByovpc() *cobra.Command {
-	config := cloudclient.CmdOptions{}
 
 	byovpcCmd := &cobra.Command{
 		Use:   "byovpc",
 		Short: "Verify subnet configuration of a specific VPC",
-		Run: func(cmd *cobra.Command, args []string) {
-			// Create logger
-			builder := ocmlog.NewStdLoggerBuilder()
-			builder.Debug(debug)
-			logger, err := builder.Build()
-			if err != nil {
-				fmt.Printf("Unable to build logger: %s\n", err.Error())
-				os.Exit(1)
-			}
-
-			ctx := context.TODO()
-
-			client, err := cloudclient.NewClient(ctx, logger, config)
-			if err != nil {
-				logger.Error(ctx, "Error creating %s cloud client: %s", config.CloudType, err.Error())
-				os.Exit(1)
-			}
-
-			err = client.ByoVPCValidator(ctx)
-			if err != nil {
-				logger.Error(ctx, err.Error())
-				os.Exit(1)
-			}
-
-			logger.Info(ctx, "Success")
-		},
+		Run:   func(cmd *cobra.Command, args []string) { return },
 	}
 
 	byovpcCmd.Flags().BoolVar(&debug, "debug", false, "If true, enable additional debug-level logging")
--- cmd/dns/cmd.go
+++ cmd/dns/cmd.go
@@ -7,46 +7,24 @@ import (
 
 	ocmlog "github.com/openshift-online/ocm-sdk-go/logging"
 	"github.com/openshift/osd-network-verifier/pkg/cloudclient"
+	"github.com/openshift/osd-network-verifier/pkg/parameters"
 	"github.com/spf13/cobra"
 )
 
+var vpcId string
+var config = cloudclient.CmdOptions{}
+
 func NewCmdValidateDns() *cobra.Command {
-	config := cloudclient.CmdOptions{}
 
 	validateDnsCmd := &cobra.Command{
-		Use: "dns",
-		Run: func(cmd *cobra.Command, args []string) {
-			// ctx
-			ctx := context.TODO()
-
-			// Create logger
-			builder := ocmlog.NewStdLoggerBuilder()
-			builder.Debug(config.Debug)
-			logger, err := builder.Build()
-			if err != nil {
-				fmt.Printf("Unable to build logger: %s\n", err.Error())
-				os.Exit(1)
-			}
-			client, err := cloudclient.NewClient(ctx, logger, config)
-			if err != nil {
-				logger.Error(ctx, "Error creating %s cloud client: %s", config.CloudType, err.Error())
-				os.Exit(1)
-			}
-			out := client.VerifyDns(ctx, config.VpcID)
-			out.Summary()
-			if !out.IsSuccessful() {
-				logger.Error(ctx, "Failure!")
-				os.Exit(1)
-			}
-
-			logger.Info(ctx, "Success")
-		},
+		Use:  "dns",
+		RunE: run,
 	}
 
-	validateDnsCmd.Flags().StringVar(&config.VpcID, "vpc-id", "", "ID of the VPC under test")
+	validateDnsCmd.Flags().StringVar(&vpcId, "vpc-id", "", "ID of the VPC under test")
 	validateDnsCmd.Flags().StringVar(&config.Region, "region", cloudclient.RegionDefault, fmt.Sprintf("Region to validate. Defaults to exported var %[1]v or '%[2]v' if not %[1]v set", cloudclient.RegionEnvVarStr, cloudclient.RegionDefault))
 	validateDnsCmd.Flags().BoolVar(&config.Debug, "debug", false, "If true, enable additional debug-level logging")
-
+	//
 	if err := validateDnsCmd.MarkFlagRequired("vpc-id"); err != nil {
 		validateDnsCmd.PrintErr(err)
 		os.Exit(1)
@@ -54,4 +32,30 @@ func NewCmdValidateDns() *cobra.Command {
 
 	return validateDnsCmd
 
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	// ctx
+	ctx := context.TODO()
+
+	// Create logger
+	builder := ocmlog.NewStdLoggerBuilder()
+	builder.Debug(config.Debug)
+	logger, err := builder.Build()
+	if err != nil {
+		return fmt.Errorf("Unable to build logger: %s\n", err.Error())
+	}
+
+	client, err := cloudclient.GetClientFor(&config)
+	if err != nil {
+		return fmt.Errorf("Error creating %s cloud client: %s", config.CloudType, err.Error())
+	}
+	out := client.VerifyDns(parameters.ValidateDns{VpcId: vpcId})
+	out.Summary()
+	if !out.IsSuccessful() {
+		return fmt.Errorf("Failure!")
+	}
+
+	logger.Info(ctx, "Success")
+	return nil
 }
