fdc254c58d15d5f9e594a5c82edd022bca925820
Emika Hammond <emhammon@redhat.com>
2024-07-16 12:46:36 -0400

    update probe param

--- cmd/egress/cmd.go
+++ cmd/egress/cmd.go
@@ -237,7 +237,6 @@ are set correctly before execution.
 	validateEgressCmd.Flags().StringVar(&config.vpcSubnetID, "subnet-id", "", "target subnet ID")
 	validateEgressCmd.Flags().StringVar(&config.cloudImageID, "image-id", "", "(optional) cloud image for the compute instance")
 	validateEgressCmd.Flags().StringVar(&config.instanceType, "instance-type", "t3.micro", "(optional) compute instance type")
-	validateEgressCmd.Flags().StringVar(&config.securityGroupId, "security-group-id", "", "(deprecated in favor of --security-group-ids)")
 	validateEgressCmd.Flags().StringSliceVar(&config.securityGroupIDs, "security-group-ids", []string{}, "(optional) comma-separated list of sec. group IDs to attach to the created EC2 instance. If absent, one will be created")
 	validateEgressCmd.Flags().StringVar(&config.region, "region", "", fmt.Sprintf("(optional) compute instance region. If absent, environment var %[1]v = %[2]v and %[3]v = %[4]v will be used", awsRegionEnvVarStr, awsRegionDefault, gcpRegionEnvVarStr, gcpRegionDefault))
 	validateEgressCmd.Flags().StringToStringVar(&config.cloudTags, "cloud-tags", map[string]string{}, "(optional) comma-seperated list of tags to assign to cloud resources e.g. --cloud-tags key1=value1,key2=value2")
--- pkg/probes/dummy/dummy.go
+++ pkg/probes/dummy/dummy.go
@@ -1,6 +1,7 @@
 package dummy
 
 import (
+	"github.com/openshift/osd-network-verifier/pkg/data/cpu"
 	"github.com/openshift/osd-network-verifier/pkg/output"
 )
 
@@ -18,7 +19,7 @@ func (prb Probe) GetStartingToken() string { return startingToken }
 func (prb Probe) GetEndingToken() string { return endingToken }
 
 // GetMachineImageID returns the string ID of the VM image to be used for the probe instance
-func (prb Probe) GetMachineImageID(string, string, string) (string, error) {
+func (prb Probe) GetMachineImageID(string, cpu.Architecture, string) (string, error) {
 	return "rhel-9", nil
 }
 
@@ -27,7 +28,7 @@ func (prb Probe) GetExpandedUserData(map[string]string) (string, error) {
 	return `#!/bin/sh
 	systemctl mask --now serial-getty@ttyS0.service
 	systemctl disable --now syslog.socket rsyslog.service
-	sysctl -w kernel.printk="0 4 0 7"
+	sysctl -w kernel.printk="0 4 0 7"ss
 	echo DUMMY_START > /dev/ttyS0
 	echo "hello world" > /dev/ttyS0
 	echo DUMMY_END > /dev/ttyS0`, nil
