e3757333e8763debf8070e7f68b0edfffd61bc49
Emika Hammond <emhammon@redhat.com>
2024-07-22 09:56:21 -0400

    change startup-script to split printing and curl
    service to print userdata begin runs multiple times despite
    succeeding. service to run curl fails. printing end token requires
    curl to succeed so it does not start running. crash recovery kernel
    arming runs everytime vm is created.

--- pkg/probes/gcp_curl/startup-script.sh
+++ pkg/probes/gcp_curl/startup-script.sh
@@ -15,23 +15,57 @@ Restart=on-failure
 WantedBy=multi-user.target
 EOF
 
-cat <<EOF > /etc/systemd/system/curl.service
+cat <<EOF > /etc/systemd/system/startcurl.service
 [Unit]
-Description=Service to verify egress
+Description=Service to print USERDATA_BEGIN
 
 [Service]
 Type=oneshot
-ExecStartPre=/bin/sh -c "${USERDATA_START} > /dev/ttyS0"
+ExecStart=echo ${USERDATA_BEGIN}
+StandardOutput=file:/dev/ttyS0
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat <<EOF > /etc/systemd/system/curl.service
+[Unit]
+Description=Service to run curl
+After=startcurl.service
+Requires=startcurl.service
+
+[Service]
+Type=oneshot
 ExecStart=curl --retry 3 --retry-connrefused -t B -Z -s -I -m ${TIMEOUT} -w "%{stderr}${LINE_PREFIX}%{json}\n" ${CURLOPT} ${URLS} --proto =http,https,telnet ${TLSDISABLED_URLS_RENDERED}
-ExecStartPost=/bin/sh -c "${USERDATA_END} > /dev/ttyS0"
 StandardOutput=file:/dev/pts/0
 StandardError=file:/dev/ttyS0
 Restart=on-failure
 
 [Install]
-WantedBy=network.target
+WantedBy=multi-user.target
 EOF
 
+cat <<EOF > /etc/systemd/system/endcurl.service
+[Unit]
+Description=Service to print USERDATA_END
+After=curl.service
+Requires=curl.service
+
+[Service]
+Type=oneshot
+ExecStart=echo ${USERDATA_END}
+StandardOutput=file:/dev/ttyS0
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sed -i '/^\[Journal\]/a ForwardToConsole=no' /etc/systemd/journald.conf
+systemctl restart systemd-journald
 systemctl daemon-reload
 systemctl start silence
+systemctl start startcurl
 systemctl start curl
+systemctl start endcurl
