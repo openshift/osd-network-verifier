4b0f63664c753de24f83d58cf2b9d5775a4004f4
Emika Hammond <emhammon@redhat.com>
2025-11-11 12:43:43 -0500

    update explicit proxy example for HCP compatability  (#331)
    * allow all traffic in proxied subnet through proxy
    
    * add s3 VPC endpoint for ROSA
    
    * add elb and ec2 interface endpoints and sg
    
    * use region id instead of deprecated name property
    
    * scale up proxy instance for networking resources
    
    * let aws create default route instead of forcing traffic through proxy

--- examples/aws/terraform/vpc-proxied-explicit/main.tf
+++ examples/aws/terraform/vpc-proxied-explicit/main.tf
@@ -105,7 +105,7 @@ resource "random_password" "proxy_webui_password" {
 # Create the proxy EC2 instance inside the public subnet
 resource "aws_instance" "proxy_machine" {
   ami               = data.aws_ami.rhel10.id
-  instance_type     = "t3.micro"
+  instance_type     = "m5.xlarge"
   key_name          = aws_key_pair.proxy_machine_key.key_name # SSH key for debugging
   availability_zone = var.availability_zone
   tags              = { Name = "${var.name_prefix}-proxy-machine" }
@@ -167,10 +167,6 @@ resource "aws_route_table" "proxied" {
   vpc_id = aws_vpc.main.id
   tags   = { Name = "${var.name_prefix}-proxied-rtb" }
 
-  route {
-    cidr_block           = var.proxied_subnet_cidr_block
-    network_interface_id = aws_instance.proxy_machine.primary_network_interface_id
-  }
   dynamic "route" {
     for_each = toset(var.proxied_subnet_escape_routes)
     content {
@@ -186,10 +182,79 @@ resource "aws_route_table_association" "proxied" {
   route_table_id = aws_route_table.proxied.id # ID of proxied route table
 }
 
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id      = aws_vpc.main.id
+  service_name = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  # Attach to the route tables of the subnets where your worker nodes are (proxied and public)
+  route_table_ids = [
+    aws_route_table.public.id,
+    aws_route_table.proxied.id
+  ]
+  tags = {
+    Name = "${var.name_prefix}-s3-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ec2" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.region}.ec2"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.interface_endpoints_sg.id,
+  ]
+
+  private_dns_enabled = true
+  subnet_ids = [aws_subnet.proxied.id]
+  tags = {
+    Name = "${var.name_prefix}-ec2-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "elasticloadbalancing" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.region}.elasticloadbalancing"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.interface_endpoints_sg.id,
+  ]
+
+  private_dns_enabled = true
+  subnet_ids = [aws_subnet.proxied.id]
+  tags = {
+    Name = "${var.name_prefix}-elb-endpoint"
+  }
+}
+
+
+resource "aws_security_group" "interface_endpoints_sg" {
+  vpc_id      = aws_vpc.main.id
+  name        = "${var.name_prefix}-interface-endpoints-sg"
+  description = "Allows inbound HTTPS from VPC to interface endpoints"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+    description = "HTTPS from VPC"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 ## OUTPUTS
 output "region" {
   description = "VPC region"
-  value       = data.aws_region.current.name
+  value       = data.aws_region.current.id
 }
 output "proxy_machine_instance_id" {
   description = "Proxy machine instance ID"
@@ -216,6 +281,10 @@ output "proxy_machine_cert_url" {
   description = "Credentialed URL for downloading the proxy CA cert (available in 2-5 minutes)"
   value       = "https://${var.proxy_webui_username}:${random_password.proxy_webui_password.result}@${aws_instance.proxy_machine.public_ip}:8443/mitmproxy-ca-cert.pem"
   sensitive   = true
+}
+output "public_subnet_id" {
+  description = "Public subnet ID"
+  value       = aws_subnet.public.id
 }
 output "proxied_subnet_id" {
   description = "Proxied subnet ID (launch your test/'captive' instances here)"
